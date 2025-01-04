4bf1b39a927a6f2d798de119fd75cbaca56bd096
G.vedansh reddy <2311cs010244@mallareddyuniversity.ac.in>
2025-01-04 10:43:02 +0530

    Add files via upload

---
+++ password.java
@@ -0,0 +1,11 @@
+public class password {
+    public static void main(String[] args) {
+        String name = "mallikarjun";
+        String pwd = "mallikarjun@123";
+        while((name == "mallikarjun")&&(pwd == "mallikarjun@123")){
+            System.out.println("hello mallikarjun welcome");
+            break;
+        }
+
+    }
+}
