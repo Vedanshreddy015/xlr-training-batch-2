102af303116b0846a25872eca423095f34036917
G.vedansh reddy <2311cs010244@mallareddyuniversity.ac.in>
2025-01-07 10:24:18 +0530

    Add files via upload

---
+++ demo015.java
@@ -0,0 +1,7 @@
+public class demo015{
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println("The value of a after swapping with b: "+b);
+        System.out.println("The value of b after swapping with a: "+a);}
+}
