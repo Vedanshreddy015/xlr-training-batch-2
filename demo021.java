ebe2ac6a20e698fd3bbd8fb38be4d48a70a2f5e6
G.vedansh reddy <2311cs010244@mallareddyuniversity.ac.in>
2025-01-20 09:56:08 +0530

    Add files via upload

---
+++ demo021.java
@@ -0,0 +1,12 @@
+public class demo021{
+    public static void main(String[] args) {
+        String[] arr = new String[4];
+        arr[0] = "tharuni";
+        arr[1] = "kavita";
+        arr[2] = "teju";
+        arr[3] = "shine";
+
+        System.err.println(arr[3]);
+
+    }
+}
