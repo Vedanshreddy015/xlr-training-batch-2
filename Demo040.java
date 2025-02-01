49b07a08676cf69a30f756726f7263bacbc0b13d
G.vedansh reddy <2311cs010244@mallareddyuniversity.ac.in>
2025-02-01 09:38:10 +0530

    Add files via upload

--- Demo040.java
+++ Demo040.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class Demo039 {
+public class Demo040 {
     public static void main(String[] args) {
         char ch;
-        
+        // Prompt the user to enter a character
         try (Scanner scanner = new Scanner(System.in)) {
-        
+            // Prompt the user to enter a character
             System.out.print("Enter a character: ");
             ch = scanner.next().charAt(0);
-            
+            // Close the scanner
         }
 
-        
+        // Check if the character is uppercase, lowercase, digit, or special character
         if (Character.isUpperCase(ch)) {
             System.out.println(ch + " is an uppercase letter.");
         } else if (Character.isLowerCase(ch)) {
@@ -19,7 +19,7 @@ public class Demo039 {
         } else if (Character.isDigit(ch)) {
             System.out.println(ch + " is a digit.");
         } else {
-            System.out.println(ch + " is not a letter or a digit.");
+            System.out.println(ch + " is a special character.");
         }
     }
 }
